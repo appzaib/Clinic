06fcc78a1ba17906f51730abfee9aaddfd344684
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2023-01-20 11:46:39 +0500

    About Page, Appointment Page & Section

---
+++ pages/about.jsx
@@ -0,0 +1,233 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <div
+        className="bg-no-repeat bg-cover bg-center w-full  py-32  px-16"
+        style={{
+          backgroundImage: `url("https://production-prod-trueprofile-blogassets-origin.s3.ap-southeast-1.amazonaws.com/uploads/2022/03/shutterstock_1367016380-1024x535.jpg")`,
+        }}
+      >
+        <h2 className="text-4xl text-white font-medium title-font w-2/4 leading-9 pb-4">
+          About USA Health
+        </h2>
+        <p className=" text-white font-medium title-font mb-2 w-2/4  leading-8">
+          In the lab and in the classroom, at our physician offices and across
+          our campuses, USA Health is Transforming Medicine™ throughout the
+          region.
+        </p>
+      </div>
+      <div class="container px-5 py-24 mx-auto">
+        <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-start">
+          <h1 class="text-gray-900 font-medium title-font tracking-wider text-3xl mb-4">
+            The Mission and Vision of USA Health
+          </h1>
+          <p class="leading-relaxed text-lg mb-4">
+            From the lab to the hospital, from fighting cancer to fighting
+            colds, medicine is ever changing at USA Health. Our culture
+            encourages curiosity and challenges assumptions as we push each
+            other to improve the health of patients. What we discover in the
+            lab, we take to the hospital. What we learn in the classroom, we
+            apply in real life. What goes on behind the scenes directs what goes
+            on in the hallways of our hospitals and in doctors' offices. It’s a
+            culture dedicated to Transforming Medicine™ in every way possible.
+          </p>
+          <h2 class="text-gray-900 font-medium title-font tracking-wider text-lg">
+            Our Mission:
+          </h2>
+          <p class="leading-relaxed text-lg mb-4">
+            We help people lead longer, better lives.
+          </p>
+          <h2 class="text-gray-900 font-medium title-font tracking-wider text-lg">
+            Our Vision:
+          </h2>
+          <p class="leading-relaxed text-lg mb-4">
+            USA Health strives to be the premier integrated, patient-centered
+            healthcare organization in our region. We educate future generations
+            of healthcare providers, and we discover innovative ways to help
+            people become healthier.
+          </p>
+        </div>
+      </div>
+      <div className="container mx-auto flex md:flex-row flex-col items-center  bg-blue-200 mt-6">
+        <div className="lg:flex-grow md:w-1/2 px-28 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-700">
+            Clinical Trials
+          </h1>
+          <p className="mb-8 leading-relaxed font-medium text-gray-700">
+            To help accelerate medical advancements and improve the lives of
+            patients, USA Health continually participates in the latest clinical
+            trials, both nationally and regionally, to develop and test new
+            medicines and treatment options. Learn more about participating in
+            one of our many ongoing clinical trials. Learn More
+          </p>
+          <div className="flex justify-center">
+            <button className="inline-flex text-white bg-blue-900 border-0 py-2 px-6 focus:outline-none rounded text-lg">
+              Learn More
+            </button>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center"
+            alt="hero"
+            src="https://www.usahealthsystem.com/uploads/ctas/_alternatingBlockMedium/Clinical-Trials.jpg?20211006111912"
+          />
+        </div>
+      </div>
+      <div className="container mx-auto flex md:flex-row flex-col items-center ">
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center"
+            alt="hero"
+            src="https://www.usahealthsystem.com/uploads/ctas/_alternatingBlockMedium/Education_and_Research.jpg?20211006111910"
+          />
+        </div>
+        <div className="lg:flex-grow md:w-1/2 px-28 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-700">
+            Clinical Trials
+          </h1>
+          <p className="mb-8 leading-relaxed font-medium text-gray-700">
+            To help accelerate medical advancements and improve the lives of
+            patients, USA Health continually participates in the latest clinical
+            trials, both nationally and regionally, to develop and test new
+            medicines and treatment options. Learn more about participating in
+            one of our many ongoing clinical trials. Learn More
+          </p>
+          <div className="flex justify-center">
+            <button className="inline-flex text-white bg-blue-900 border-0 py-2 px-6 focus:outline-none rounded text-lg">
+              Learn More
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="container mx-auto flex md:flex-row flex-col items-center  bg-blue-200 mt-6">
+        <div className="lg:flex-grow md:w-1/2 px-28 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-700">
+            Clinical Trials
+          </h1>
+          <p className="mb-8 leading-relaxed font-medium text-gray-700">
+            To help accelerate medical advancements and improve the lives of
+            patients, USA Health continually participates in the latest clinical
+            trials, both nationally and regionally, to develop and test new
+            medicines and treatment options. Learn more about participating in
+            one of our many ongoing clinical trials. Learn More
+          </p>
+          <div className="flex justify-center">
+            <button className="inline-flex text-white bg-blue-900 border-0 py-2 px-6 focus:outline-none rounded text-lg">
+              Learn More
+            </button>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center"
+            alt="hero"
+            src="https://www.usahealthsystem.com/uploads/ctas/_alternatingBlockMedium/Clinical-Trials.jpg?20211006111912"
+          />
+        </div>
+      </div>
+      <div className="container mx-auto flex md:flex-row flex-col items-center ">
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center"
+            alt="hero"
+            src="https://www.usahealthsystem.com/uploads/ctas/_alternatingBlockMedium/Education_and_Research.jpg?20211006111910"
+          />
+        </div>
+        <div className="lg:flex-grow md:w-1/2 px-28 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-700">
+            Clinical Trials
+          </h1>
+          <p className="mb-8 leading-relaxed font-medium text-gray-700">
+            To help accelerate medical advancements and improve the lives of
+            patients, USA Health continually participates in the latest clinical
+            trials, both nationally and regionally, to develop and test new
+            medicines and treatment options. Learn more about participating in
+            one of our many ongoing clinical trials. Learn More
+          </p>
+          <div className="flex justify-center">
+            <button className="inline-flex text-white bg-blue-900 border-0 py-2 px-6 focus:outline-none rounded text-lg">
+              Learn More
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="container mx-auto flex md:flex-row flex-col items-center  bg-blue-200 mt-6">
+        <div className="lg:flex-grow md:w-1/2 px-28 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-700">
+            Clinical Trials
+          </h1>
+          <p className="mb-8 leading-relaxed font-medium text-gray-700">
+            To help accelerate medical advancements and improve the lives of
+            patients, USA Health continually participates in the latest clinical
+            trials, both nationally and regionally, to develop and test new
+            medicines and treatment options. Learn more about participating in
+            one of our many ongoing clinical trials. Learn More
+          </p>
+          <div className="flex justify-center">
+            <button className="inline-flex text-white bg-blue-900 border-0 py-2 px-6 focus:outline-none rounded text-lg">
+              Learn More
+            </button>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center"
+            alt="hero"
+            src="https://www.usahealthsystem.com/uploads/ctas/_alternatingBlockMedium/Clinical-Trials.jpg?20211006111912"
+          />
+        </div>
+      </div>
+      <div className="container mx-auto flex md:flex-row flex-col items-center ">
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center"
+            alt="hero"
+            src="https://www.usahealthsystem.com/uploads/ctas/_alternatingBlockMedium/Education_and_Research.jpg?20211006111910"
+          />
+        </div>
+        <div className="lg:flex-grow md:w-1/2 px-28 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-700">
+            Clinical Trials
+          </h1>
+          <p className="mb-8 leading-relaxed font-medium text-gray-700">
+            To help accelerate medical advancements and improve the lives of
+            patients, USA Health continually participates in the latest clinical
+            trials, both nationally and regionally, to develop and test new
+            medicines and treatment options. Learn more about participating in
+            one of our many ongoing clinical trials. Learn More
+          </p>
+          <div className="flex justify-center">
+            <button className="inline-flex text-white bg-blue-900 border-0 py-2 px-6 focus:outline-none rounded text-lg">
+              Learn More
+            </button>
+          </div>
+        </div>
+      </div>
+      <div
+        className="bg-no-repeat bg-cover bg-center w-full  py-32  px-16"
+        style={{
+          backgroundImage: `url("https://www.usahealthsystem.com/uploads/ctas/_largeCta/Request-an-Appointment-Background.jpg")`,
+          backgroundColor: "rgba(0, 0, 255, 0.5)",
+        }}
+      >
+        <div className=" text-white font-medium title-font m-auto text-center px-[6rem] py-[3rem] z-[1] relative">
+          <h2 className="text-4xl mb-[20px]">Contact USA Health</h2>
+          <p className="mb-[3rem] leading-8 px-[9rem] text-lg">
+            USA Health is here for you. Reach out to us if you have a question
+            about billing, research, volunteering, insurance or even just want
+            to tell us about your experience at USA Health. You can expect to
+            hear from us soon.
+          </p>
+          <button className="inline-flex text-white bg-blue-900 border-0 py-2 px-6 focus:outline-none rounded text-lg">
+            Contact Us
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/section.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+import { RiHandHeartLine } from "react-icons/ri";
+
+const Feature = () => {
+  return (
+    <div className="flex justify-center  p-4 bg-blue-900 ">
+      <div className="flex flex-col justify-center items-center ">
+        <div className="text-white text-3xl md:text-5xl font-small">
+          <h1>Why Choose Clinic?</h1>
+          <div className=" w-48 h-1 bg-white rounded mt-4 mb-4"></div>
+        </div>
+
+        <div className="flex flex-col md:flex-row max-w-7xl justify-center items-center ">
+          <div className="overflow-hidden w-full m-4 flex justify-center rounded-lg  md:w-[33%] px-8">
+            <div className="flex flex-col md:flex-row items-center justify-center  ">
+              <div className="  items-center justify-center flex py-2">
+                <div className="flex flex-col  items-center justify-center ">
+                  <div>
+                    <RiHandHeartLine className="text-white w-12 h-12 hover:cursor-pointer md:-translate-x-20 hover:-translate-y-4 mt-3 transition duration-300 delay-150 hover:delay-300" />
+                  </div>
+                  <div className="text-white  m-2">
+                    <h1 className="sm:text-3xl text-2xl font-small title-font text-white mb-4">
+                      More Experience
+                    </h1>
+
+                    <p className="leading-relaxed text-base mb-4">
+                      The million patients we treat each year prepares us to
+                      treat the one who matters most — you.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="overflow-hidden w-full m-4 flex justify-center rounded-lg  md:w-[33%] px-8">
+            <div className="flex flex-col md:flex-row items-center justify-center  ">
+              <div className="  items-center justify-center flex py-2">
+                <div className="flex flex-col  items-center justify-center ">
+                  <div>
+                    <RiHandHeartLine className="text-white w-12 h-12 hover:cursor-pointer md:-translate-x-20 hover:-translate-y-4 mt-3 transition duration-300 delay-150 hover:delay-300" />
+                  </div>
+                  <div className="text-white  m-2">
+                    <h1 className="sm:text-3xl text-2xl font-small title-font text-white mb-4">
+                      More Experience
+                    </h1>
+                    <p className="leading-relaxed text-base mb-4">
+                      The million patients we treat each year prepares us to
+                      treat the one who matters most — you.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="overflow-hidden w-full m-4 flex justify-center rounded-lg  md:w-[33%] px-8">
+            <div className="flex flex-col md:flex-row items-center justify-center  ">
+              <div className="  items-center justify-center flex py-2">
+                <div className="flex flex-col  items-center justify-center ">
+                  <div>
+                    <RiHandHeartLine className="text-white w-12 h-12 hover:cursor-pointer md:-translate-x-20 hover:-translate-y-4 mt-3 transition duration-300 delay-150 hover:delay-300" />
+                  </div>
+                  <div className="text-white  m-2">
+                    <h1 className="sm:text-3xl text-2xl font-small title-font text-white mb-4">
+                      More Experience
+                    </h1>
+                    <p className="leading-relaxed text-base mb-4">
+                      The million patients we treat each year prepares us to
+                      treat the one who matters most — you.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="overflow-hidden w-full m-4 flex justify-center rounded-lg  md:w-[33%] px-8">
+            <div className="flex flex-col md:flex-row items-center justify-center  ">
+              <div className="  items-center justify-center flex py-2">
+                <div className="flex flex-col  items-center justify-center ">
+                  <div>
+                    <RiHandHeartLine className="text-white w-12 h-12 hover:cursor-pointer md:-translate-x-20 hover:-translate-y-4 mt-3 transition duration-300 delay-150 hover:delay-300" />
+                  </div>
+                  <div className="text-white  m-2">
+                    <h1 className="sm:text-3xl text-2xl font-small title-font text-white mb-4">
+                      More Experience
+                    </h1>
+                    <p className="leading-relaxed text-base mb-4">
+                      The million patients we treat each year prepares us to
+                      treat the one who matters most — you.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Feature;
+
+{
+  /* <div className="container flex flex-wrap">
+  <div
+    className="bg-no-repeat bg-cover bg-center w-full bg-opacity-70"
+    style={{
+      backgroundImage: `url("https://production-prod-trueprofile-blogassets-origin.s3.ap-southeast-1.amazonaws.com/uploads/2022/03/shutterstock_1367016380-1024x535.jpg")`,
+    }}
+  >
+    <h2 className="text-4xl  px-28  py-32 text-white font-medium title-font mb-2 w-2/4 leading-9">
+      USA Health is Transforming Medicine along the Gulf Coast to care for the
+      unique needs of our community.
+    </h2>
+  </div>
+  <div className="text-2xl px-5 py-8 font-medium border-r-2 w-2/4 text-center hover:bg-blue-900 hover:text-white cursor-pointer">
+    Request An Appointment
+  </div>
+
+  <div className="w-2/4 text-2xl px-5 py-8 font-medium border-r-2  text-center hover:bg-blue-900 hover:text-white  cursor-pointer">
+    Refer A Patient
+  </div>
+</div>; */
+}
