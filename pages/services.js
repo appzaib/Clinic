c8cf150151908f182187e431cd5622267a49c467
abdullah-k <abdullah771.ak@gmail.com>
2023-02-03 00:35:38 +0500

    add .

--- pages/services.js
+++ pages/services.js
@@ -42,6 +42,28 @@ export default function Services(){
       <span className="uppercase">medical services</span>
       <h1 className="text-6xl text-blue-800 font-bold py-5" >Price And Plans</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga perferendis necessitatibus ratione at perspiciatis sed deleniti magni aspernatur obcaecati reprehenderit rem qui aut accusamus voluptatem, in quibusdam molestiae vitae est.</p>
+      <div className="w-full flex flex-wrap items-center py-20">
+        <div className="lg:w-1/3 w-full  flex flex-wrap flex-col px-1">
+          <span className="text-blue-900 text-2xl font-semibold">Silver Pack</span>
+          <h1 className="text-blue-900 text-6xl font-extrabold py-5">29.00$</h1>
+          <span className="text-sm text-gray-500">Per Month</span>
+          <button className="px-10 py-4 mt-10 bg-blue-700 hover:bg-amber-600 duration-300 text-xl text-white font-semibold">Get Now</button>
+        </div>
+        <div className="lg:w-1/3 w-full flex flex-wrap flex-col px-1">
+          <span className="text-blue-900 text-2xl font-semibold">Gold Pack</span>
+          <h1 className="text-blue-900 text-6xl font-extrabold py-5">49.00$</h1>
+          <span className="text-sm text-gray-500">Per Month</span>
+          <button className="px-10 py-4 mt-10 bg-blue-700 hover:bg-amber-600 duration-300 text-xl text-white font-semibold">Get Now</button>
+        </div>
+        <div className="lg:w-1/3 w-full flex flex-wrap flex-col px-1">
+          <span className="text-blue-900 text-2xl font-semibold">Platinum Pack</span>
+          <h1 className="text-blue-900 text-6xl font-extrabold py-5">69.00$</h1>
+          <span className="text-sm text-gray-500">Per Month</span>
+          <button className="px-20 py-4 mt-10 bg-blue-700 hover:bg-amber-600 duration-300 text-xl text-white font-semibold text-center">Get Now</button>
+        </div>
+
+
+      </div>
     </div>
   </div>
 )
