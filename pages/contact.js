f9c6ac1cf85267daaafd7649845ec674c8f6ee79
abdullah-k <abdullah771.ak@gmail.com>
2023-02-05 22:22:27 +0500

    add fixes of contact pages

--- pages/contact.js
+++ pages/contact.js
@@ -8,12 +8,13 @@ export default function Contact(){
     return(
         <>
        <div class="justify-center flex">
-<div class="w-full my-5 lg:mx-auto flex flex-col items-center ">
-    <div class="border md:w-[80%] h-full bg-cyan-400 text-white p-10"> <span>
+<div class="w-full my-5 lg:mx-auto flex flex-col items-center bg-cyan-400">
+    <div class="md:w-4/5 h-full  text-white p-10"> <span>
             <h1 class="text-3xl font-semibold text-center mb-4 text-yellow-300">Contact Us</h1>
             <p class="text-xs font-normal mt-2 text-center">Contact us by filling this contact form So we shell be in touch with you.</p>
         </span>
-        <div class="mt-6 leading-7 text-center"> <input type="text" name="" placeholder="Full Name" id=""
+        <div class="mt-6 leading-7 text-center"> 
+            <input type="text" name="" placeholder="Full Name" id=""
                 class="border-b-2 w-[80%] mb-4 p-1 border-b-white placeholder:text-white placeholder:opacity-80 text-sm bg-cyan-400 focus:outline-none"/>
             <input type="email" name="" placeholder="Email Address" id=""
                 class="border-b-2 w-[80%] mb-4 p-1 border-b-white placeholder:text-white placeholder:opacity-80 text-sm bg-cyan-400 focus:outline-none"/>
