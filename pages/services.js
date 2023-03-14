053a169203b07c224138c66ff120abff58e56e34
abdullah khan <abdullah771.ak@gmail.com>
2023-03-14 14:17:28 -0700

    add slide

--- pages/services.js
+++ pages/services.js
@@ -7,31 +7,31 @@ export default function Services(){
 
   return(
   <div className="flex flex-col items-center">
-    <h1 className="text-6xl py-40 font-semibold hover:translate-y-20 duration-1000 text-blue-900">Services</h1>
+    <h1 className="text-6xl py-20 font-semibold text-blue-900">Services</h1>
 
     <div className="container lg:pl-10 lg:w-full flex flex-wrap justify-center">
-      <div className="w-full md:w-1/4 lg:w-1/4 hover:translate-x-20 duration-1000 hover:cursor-pointer ">
+      <div className="w-full md:w-1/4 lg:w-1/4 hover:-translate-y-10 duration-500 hover:cursor-pointer ">
         <div className="flex flex-col items-center ">
           <FaRegHeart className="text-6xl text-sky-900"/>
           <h1 className="py-4 text-lg font-semibold text-sky-800 uppercase">cardiology</h1>
         </div>
       </div>
 
-      <div className="w-full md:w-1/4 lg:w-1/4 hover:translate-x-20 duration-1000 hover:cursor-pointer ">
+      <div className="w-full md:w-1/4 lg:w-1/4 hover:-translate-y-10 duration-500 hover:cursor-pointer ">
         <div className="flex flex-col items-center ">
           <FaMicroscope className="text-6xl text-sky-900"/>
           <h1 className="py-4 text-lg font-semibold text-sky-800 uppercase">diagnostic</h1>
         </div>
       </div>
 
-      <div className="w-full md:w-1/4 lg:w-1/4 hover:translate-x-20 duration-1000 hover:cursor-pointer ">
+      <div className="w-full md:w-1/4 lg:w-1/4 hover:-translate-y-10 duration-500 hover:cursor-pointer ">
         <div className="flex flex-col items-center ">
           <FaViruses className="text-6xl text-sky-900"/>
           <h1 className="py-4 text-lg font-semibold text-sky-800 uppercase">virology</h1>
         </div>
       </div>
 
-      <div className="w-full md:w-1/4 lg:w-1/4 hover:translate-x-20 duration-1000 hover:cursor-pointer ">
+      <div className="w-full md:w-1/4 lg:w-1/4 hover:-translate-y-10 duration-500 hover:cursor-pointer ">
         <div className="flex flex-col items-center ">
           <FaStethoscope className="text-6xl text-sky-900"/>
           <h1 className="py-4 text-lg font-semibold text-sky-800 uppercase">therapy</h1>
